864cf03b767991306684ac7879e98f773c3bbb6c
user1990 <paulius.rimg1990@gmail.com>
2017-07-04 16:08:10 +0300

    # Files for Translator with Yandex API

---
+++ 03 - Translator/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-center" id="app">
+    <h1>Word Translator</h1>
+    <h5 class="text-muted">Powered By Vue.js</h5>
+    <hr>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+    <TranslateOutput v-text="translatedText"></TranslateOutput>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput';
+
+export default {
+  name: 'app',
+  components:{
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function(){
+    return {
+      translatedText:''
+    }
+  },
+  methods: {
+    translateText:function(text, language){
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170704T122158Z.46c47b9ce570731e.2753b6d8cf16119240ac9e5566b9cc091abb965f&lang='+language+'&text='+text)
+      .then((response) => {
+        this.translatedText = response.body.text[0];
+      });
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background:#fefefe;
+}
+</style>
