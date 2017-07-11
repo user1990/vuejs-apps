fd1745171f8c48a8d55cc328b0207bfd9bf46976
user1990 <paulius.rimg1990@gmail.com>
2017-07-11 14:35:08 +0300

    # Add, delete, props, slots, progress bar

--- 05 - Quotes/src/App.vue
+++ 05 - Quotes/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="container">
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quate to delete it!</div>
+      </div>
+    </div>
   </div>
 </template>
 
-
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 
 export default {
   data: function() {
@@ -17,8 +25,21 @@ export default {
       maxQuotes: 10
     }
   },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('Please delete Quotes first!');
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
   components: {
-    'app-quote-grid': QuoteGrid
+    'app-quote-grid': QuoteGrid,
+    'app-new-quote': NewQuote,
+    'app-header': Header
   }
 }
 </script>
--- 05 - Quotes/src/components/QuoteGrid.vue
+++ 05 - Quotes/src/components/QuoteGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+    <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
   </div>
 </template>
 
@@ -11,6 +11,11 @@
     props: ['quotes'],
     components: {
       'app-quote': Quote
+    },
+    methods: {
+      deleteQuote(index) {
+        this.$emit('quoteDeleted', index);
+      }
     }
   }
 </script>
