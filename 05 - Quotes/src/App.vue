34db38886b39fb4c52c4ef11d361c6e555130e85
user1990 <paulius.rimg1990@gmail.com>
2017-07-11 12:52:30 +0300

    # Starter files

---
+++ 05 - Quotes/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+
+export default {
+  data: function() {
+    return {
+      quotes: [
+        'Just a Quote to see someting!'
+      ],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    'app-quote-grid': QuoteGrid
+  }
+}
+</script>
+
+<style>
+</style>
+
---
+++ 05 - Quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue'
+
+  export default {
+    props: ['quotes'],
+    components: {
+      'app-quote': Quote
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
+
