e29d72f45fdb69bb4a64c13cf39dc93c03e46a9a
user1990 <paulius.rimg1990@gmail.com>
2017-07-28 07:23:56 +0300

    # Manage State with Vuex

--- 12 - Meetup BIG/src/components/meetup/Meetup.vue
+++ 12 - Meetup BIG/src/components/meetup/Meetup.vue
@@ -4,25 +4,33 @@
       <v-flex xs12>
         <v-card>
           <v-card-title>
-            <h6 class="info--text">My Meetup</h6>
+            <h6 class="info--text">{{ meetup.title }}</h6>
           </v-card-title>
           <v-card-media
-            src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
-            height="130px"
+            :src="meetup.imageUrl"
+            height="400px"
           ></v-card-media>
           <v-card-text>
-            <div class"info--text">30 July 2017 - where it takes place</div>
-            <div>
-              The DC Tech Meetup convenes technologists, entrepreneurs, investors and the broader
-              innovation community regularly to learn and share. Each month, 400+ innovators gather
-              to see demos, launch products and meet their future co-founders, partners and funders.
-            </div>
+            <div class="info--text">{{ meetup.date }} - Where it takes place</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda at deserunt dolorum eius esse eum, expedita hic illo ipsum magni, nobis, odio odit quaerat quibusdam quis rem veritatis voluptas!</div>
           </v-card-text>
           <v-card-actions>
-            <v-btn class="info">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn class="primary">Register</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    props: ['id'],
+    computed: {
+      meetup () {
+        return this.$store.getters.loadedMeetup(this.id)
+      }
+    }
+  }
+</script>
