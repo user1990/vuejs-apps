553d8fd4c845983388941fde90f65423f49b465a
user1990 <paulius.rimg1990@gmail.com>
2017-08-01 11:16:56 +0300

    # Adding a "Create Meetup" Form

--- 12 - Meetup BIG/src/components/meetup/CreateMeetup.vue
+++ 12 - Meetup BIG/src/components/meetup/CreateMeetup.vue
@@ -1,5 +1,112 @@
 <template>
-  <div>
-    <p>The Create Meetup Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h2 class="info--text">Create new meetup</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12>
+        <form @submit.prevent="onCreateMeetup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="title"
+                label="Title"
+                id="title"
+                v-model="title"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                id="location"
+                v-model="location"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                id="image-url"
+                v-model="imageUrl"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" height="150">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="description"
+                label="Description"
+                id="description"
+                v-model="description"
+                multiLine
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn
+                class="info"
+                type="submit"
+                :disabled="!formIsValid">Create Meetup
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: ''
+    }
+  },
+  computed: {
+    formIsValid () {
+      return this.title !== '' &&
+             this.location !== '' &&
+             this.imageUrl !== '' &&
+             this.description !== ''
+    }
+  },
+  methods: {
+    onCreateMeetup () {
+      if (!this.formIsValid) {
+        return
+      }
+      const meetupData = {
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+        date: new Date()
+      }
+      this.$store.dispatch('createMeetup', meetupData)
+      this.$router.push('/meetups')
+    }
+  }
+}
+</script>
+
