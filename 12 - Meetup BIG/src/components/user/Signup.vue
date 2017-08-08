a0c1b94d3d037ad18f784718aea5c83e71009d8e
user1990 <paulius.rimg1990@gmail.com>
2017-08-08 09:25:41 +0300

    # Added signup page, firebase

--- 12 - Meetup BIG/src/components/user/Signup.vue
+++ 12 - Meetup BIG/src/components/user/Signup.vue
@@ -1,5 +1,77 @@
 <template>
-  <div>
-    <p>The Signup Page</p>
-  </div>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card-text>
+          <v-container>
+            <form>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="email"
+                    label="Mail"
+                    id="email"
+                    v-model="email"
+                    type="email"
+                    required>
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="password"
+                    label="Password"
+                    id="password"
+                    v-model="password"
+                    type="password"
+                    required>
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    id="confirmPassword"
+                    v-model="confirmPassword"
+                    type="password"
+                    :rules="[comparePasswords]">
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn type="submit">Sign up</v-btn>
+                </v-flex>
+              </v-layout>
+            </form>
+          </v-container>
+        </v-card-text>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+    }
+  },
+  methods: {
+    onSignup () {
+      console.log({email: this.email, password: this.password, confirmPassword: this.confirmPassword})
+    }
+  }
+}
+</script>
--- 12 - Meetup BIG/src/main.js
+++ 12 - Meetup BIG/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDi_bDvfpUxs811I5BJp9S20FT62rcRbL0',
+      authDomain: 'annular-text-157306.firebaseapp.com',
+      databaseURL: 'https://annular-text-157306.firebaseio.com',
+      projectId: 'annular-text-157306',
+      storageBucket: 'annular-text-157306.appspot.com'
+    })
+  }
 })
